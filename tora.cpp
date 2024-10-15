76b36e5f03b1c86a7587f9b452c478f2f88ff463
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-15 17:02:31 +0100

    open/close

--- tora.cpp
+++ tora.cpp
@@ -2,6 +2,14 @@
 #pragma leco add_impl sqlite3
 #include "sqlite3.h"
 
-int main() {
-}
+import silog;
 
+int main() try {
+  sqlite3 * db;
+
+  if (SQLITE_OK != sqlite3_open(":memory:", &db)) silog::die("failed to open DB: %s", sqlite3_errmsg(db));
+
+  sqlite3_close(db);
+} catch (...) {
+  return 1;
+}
