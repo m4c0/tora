3bf2409b3d06c2c9ab97b6e68cd8d565cbff8b8c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-15 17:05:44 +0100

    raii for open/close

--- tora.cpp
+++ tora.cpp
@@ -2,14 +2,24 @@
 #pragma leco add_impl sqlite3
 #include "sqlite3.h"
 
+import hai;
 import silog;
 
+struct deleter {
+  void operator()(sqlite3 * db) { sqlite3_close(db); }
+};
+
+class db {
+  hai::value_holder<sqlite3 *, deleter> m_db {};
+
+public:
+  explicit db(const char * file) {
+    if (SQLITE_OK != sqlite3_open(file, &*m_db)) silog::die("failed to open DB: %s", sqlite3_errmsg(*m_db));
+  };
+};
+
 int main() try {
-  sqlite3 * db;
-
-  if (SQLITE_OK != sqlite3_open(":memory:", &db)) silog::die("failed to open DB: %s", sqlite3_errmsg(db));
-
-  sqlite3_close(db);
+  db db { ":memory:" };
 } catch (...) {
   return 1;
 }
