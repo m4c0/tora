5015705ed7b24356d390eafcfaa548b9a613c754
Eduardo Costa <m4c0@users.noreply.github.com>
2024-10-16 10:20:30 +0100

    some command-line parsing

--- tora.cpp
+++ tora.cpp
@@ -3,6 +3,7 @@
 #include "sqlite3.h"
 
 import hai;
+import jute;
 import silog;
 
 struct deleter {
@@ -52,25 +53,45 @@ public:
   }
 };
 
-int main() try {
-  db db { ":memory:" };
+class args {
+  int m_c;
+  char ** m_v;
+public:
+  args(int argc, char ** argv) : m_c { argc - 1 }, m_v { argv + 1 } {};
 
-  db.exec(R"(
-    CREATE TABLE notification (
-      id           INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-      created_at   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-      dismissed_at DATETIME,
-      text         TEXT NOT NULL
-    );
-  )");
-  db.exec("INSERT INTO notification (text) VALUES ('a')");
-  db.exec("INSERT INTO notification (text) VALUES ('b')");
-  db.exec("INSERT INTO notification (text) VALUES ('c')");
-  db.exec("INSERT INTO notification (text) VALUES ('d')");
+  jute::view take() {
+    return m_c-- ? jute::view::unsafe(*m_v++) : jute::view {};
+  }
+};
 
-  auto stmt = db.prepare("SELECT * FROM notification");
-  while (stmt.step()) {
-    silog::log(silog::info, "%d %s %s", stmt.column_int(0), stmt.column_text(1), stmt.column_text(3));
+int main(int argc, char ** argv) try {
+  args args { argc, argv };
+
+  db db { "out/.tora" };
+
+  auto cmd = args.take();
+  if (cmd == "") cmd = "list";
+
+  if (cmd == "init") {
+    db.exec(R"(
+      CREATE TABLE notification (
+        id           INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        created_at   DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        dismissed_at DATETIME,
+        text         TEXT NOT NULL
+      );
+    )");
+    db.exec("INSERT INTO notification (text) VALUES ('a')");
+    db.exec("INSERT INTO notification (text) VALUES ('b')");
+    db.exec("INSERT INTO notification (text) VALUES ('c')");
+    db.exec("INSERT INTO notification (text) VALUES ('d')");
+  } else if (cmd == "list") {
+    auto stmt = db.prepare("SELECT * FROM notification");
+    while (stmt.step()) {
+      silog::log(silog::info, "%d %s %s", stmt.column_int(0), stmt.column_text(1), stmt.column_text(3));
+    }
+  } else {
+    silog::die("unknown command %s", cmd.begin());
   }
 } catch (...) {
   return 1;
