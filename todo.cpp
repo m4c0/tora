77e9dc84d621019476bbb862b9f0d8395ed95c6f
Eduardo Costa <m4c0@users.noreply.github.com>
2025-03-07 11:29:32 +0000

    todo: emoji convention for flag

--- todo.cpp
+++ todo.cpp
@@ -47,10 +47,12 @@ int main(int argc, char ** argv) try {
       if (odd) printf("\e[48;5;232m");
       odd = !odd;
 
+      auto flag = reinterpret_cast<const char *>(stmt.column_text(3));
+
       printf("\e[38;5;238m%s %4d \e[39m%s %s\e[0K\n",
           stmt.column_text(1),
           stmt.column_int(0),
-          stmt.column_text(3),
+          flag ? flag : "  ",
           stmt.column_text(4));
       printf("\e[0m\e[2K");
     }
